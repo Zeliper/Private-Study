7100aadcb638f6dbc52318f94d6fb08e6483f700
Zeliper <dhtmddn13213@naver.com>
2023-08-23 10:42:56 +0900

    Create Cubic Bezier.js

---
+++ Cubic Bezier.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  let v = true;
+  background(220);
+  circle(200,200,5);
+  circle(mouseX,mouseY,5);
+  circle((mouseX + 200)/2,mouseY,5);
+  circle((mouseX + 200)/2,200,5);
+  circle(mouseX,(mouseY + 200)/2,5);
+  circle(200,(mouseY + 200)/2,5);
+  if(v){
+    bezier(200,200,200,(mouseY + 200)/2,mouseX,(mouseY + 200)/2,mouseX,mouseY)
+  }else{
+    bezier(200,200,(mouseX + 200)/2,200,(mouseX + 200)/2,mouseY,mouseX,mouseY)
+  }
+}
